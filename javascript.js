0308966441bbe1d2cc778d960322b1711302fc1c
justutpal <kumarutpal2112001@gmail.com>
2024-03-21 15:53:57 +0530

    Added javascript

---
+++ javascript.js
@@ -0,0 +1,26 @@
+let display = document.querySelector("#display");
+let buttons = document.querySelectorAll("button");
+let string = ""
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.innerHTML == "AC") {
+            string = ""
+            display.value = string
+        }
+
+        else if (button.innerHTML == "=") {
+            string = eval(string)
+            display.value = string
+        }
+
+        else if (button.innerHTML == "del") {
+            string = string.substring(0, string.length - 1)
+            display.value = string
+        }
+
+        else {
+            display.value += button.innerHTML
+        }
+    })
+})
